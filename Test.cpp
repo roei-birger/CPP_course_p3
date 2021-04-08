7e9f25c6b1402200bc8707f8a28db1fd3989045a
roei-birger <roei1237@gmail.com>
2021-04-08 16:56:15 +0300

    Test upload

--- Test.cpp
+++ Test.cpp
@@ -35,7 +35,7 @@ NumberWithUnits u8{1000, "g"}; //1 kg
 NumberWithUnits u9{500, "kg"};
 NumberWithUnits u10{1200, "kg"}; //1.2 ton
 NumberWithUnits u11{1.5, "ton"};
-NumberWithUnits u12{1.5, "ton"};
+NumberWithUnits u12{2, "ton"};
 NumberWithUnits u13{30, "sec"};
 NumberWithUnits u14{60, "sec"}; //1 min
 NumberWithUnits u15{30, "min"};
@@ -47,9 +47,179 @@ NumberWithUnits u20{1, "ILS"};
 NumberWithUnits u21{2, "USD"};
 NumberWithUnits u22{6, "USD"};
 
-//Basic test
-TEST_CASE("Compare function")
+//Basic tests
+TEST_CASE("Equal function")
 {
+    CHECK_EQ(u9, u9);
+    CHECK_EQ(u5, u6);
+    CHECK_EQ(u6, u5);
+    CHECK_EQ(u16, u15);
+    CHECK_EQ(u15, u16);
+    CHECK_EQ(u17, u16);
+    CHECK_EQ(u16, u17);
+}
 
-    CHECK(1 == 1);
+TEST_CASE("Equal function diff type")
+{
+    CHECK_THROWS(u12.operator==(u5)); //diff type
+    CHECK_THROWS(u5.operator==(u12));
+}
+
+TEST_CASE("not equal function")
+{
+    CHECK_NE(u1, u2); //same type
+    CHECK_NE(u2, u1);
+}
+
+TEST_CASE("not equal function diff type")
+{
+    CHECK_THROWS(u11.operator!=(u4)); //diff type
+    CHECK_THROWS(u4.operator!=(u11));
+}
+
+TEST_CASE("Less equal function")
+{
+    CHECK_LE(u1, u2); //same type
+    CHECK_LE(u2, u1);
+    CHECK_LE(u3, u4);
+    CHECK_LE(u11, u12);
+    CHECK_LE(u9, u9);
+}
+
+TEST_CASE("Less equal function diff type")
+{
+    CHECK_THROWS(u11.operator<=(u4)); //diff type
+    CHECK_THROWS(u4.operator<=(u11));
+}
+
+TEST_CASE("Less than function")
+{
+    CHECK_LT(u3, u4); //same type
+    CHECK_LT(u11, u12);
+}
+
+TEST_CASE("Less than function diff type")
+{
+    CHECK_THROWS(u11.operator<(u4)); //diff type
+    CHECK_THROWS(u4.operator<(u11));
+}
+
+TEST_CASE("greater equal function")
+{
+    CHECK_GE(u1, u2); //same type
+    CHECK_GE(u2, u1);
+    CHECK_GE(u4, u3);
+    CHECK_GE(u12, u11);
+    CHECK_GE(u9, u9);
+}
+
+TEST_CASE("greater  equal function diff type")
+{
+    CHECK_THROWS(u11.operator>=(u4)); //diff type
+    CHECK_THROWS(u4.operator>=(u11));
+}
+
+TEST_CASE("greater than function")
+{
+    CHECK_GT(u4, u3); //same type
+    CHECK_GT(u12, u11);
+}
+
+TEST_CASE("greater than function diff type")
+{
+    CHECK_THROWS(u11.operator>(u4)); //diff type
+    CHECK_THROWS(u4.operator>(u11));
+}
+
+TEST_CASE("plus function same type")
+{
+    CHECK_EQ(u15 + u16, NumberWithUnits(60, "min"));
+    CHECK_EQ(u16 + u15, NumberWithUnits(60, "min"));
+}
+
+TEST_CASE("plus function connect type")
+{
+    CHECK_EQ(u15 + u16, u18);
+    CHECK_EQ(u16 + u15, u18);
+}
+
+TEST_CASE("plus function unconnected type")
+{
+    CHECK_THROWS(u11 + u4); //diff type
+    CHECK_THROWS(u4 + u11);
+}
+
+TEST_CASE("plus function same type")
+{
+    NumberWithUnits temp_min_p(30, "min");
+    temp_min_p += NumberWithUnits(30, "min");
+    CHECK_EQ(temp_min_p, NumberWithUnits(60, "min"));
+
+    NumberWithUnits temp_km_p(3, "km");
+    temp_km_p += NumberWithUnits(3, "km");
+    CHECK_EQ(temp_km_p, NumberWithUnits(6, "km"));
+}
+
+TEST_CASE("plus function connect type")
+{
+    NumberWithUnits temp_min_p(30, "min");
+    temp_min_p += NumberWithUnits(1, "hour");
+    CHECK_EQ(temp_min_p, NumberWithUnits(90, "min"));
+
+    NumberWithUnits temp_hour_p(1, "hour");
+    temp_hour_p += NumberWithUnits(30, "min");
+    CHECK_EQ(temp_hour_p, NumberWithUnits(1.5, "hour"));
+}
+
+TEST_CASE("plus function unconnected type")
+{
+    CHECK_THROWS(u11 += u4); //diff type
+    CHECK_THROWS(u4 += u11);
+}
+
+
+TEST_CASE("minus function same type")
+{
+    CHECK_EQ(u15 - u16, NumberWithUnits(0, "min"));
+    CHECK_EQ(u16 - u15, NumberWithUnits(0, "min"));
+}
+
+TEST_CASE("minus function connect type")
+{
+    CHECK_EQ(u15 - u16, NumberWithUnits(0, "hour"));
+    CHECK_EQ(u16 - u15, NumberWithUnits(0, "hour"));
+}
+
+TEST_CASE("minus function unconnected type")
+{
+    CHECK_THROWS(u11 - u4); //diff type
+    CHECK_THROWS(u4 - u11);
+}
+
+TEST_CASE("minus equal function same type")
+{
+    NumberWithUnits temp_min_m(30, "min");
+    temp_min_m -= NumberWithUnits(0, "min");
+    CHECK_EQ(temp_min_m, NumberWithUnits(30, "min"));
+
+    NumberWithUnits temp_km_m(3, "km");
+    temp_km_m -= NumberWithUnits(2, "km");
+    CHECK_EQ(temp_km_m, NumberWithUnits(1, "km"));
+}
+
+TEST_CASE("minus equal function connect type")
+{
+    NumberWithUnits temp_min_m(30, "min");
+    temp_min_m -= NumberWithUnits(0.25, "hour");
+    CHECK_EQ(temp_min_m, NumberWithUnits(15, "min"));
+
+    NumberWithUnits temp_hour_m(1, "hour");
+    temp_hour_m -= NumberWithUnits(30, "min");
+    CHECK_EQ(temp_hour_m, NumberWithUnits(0.5, "hour"));
+}
+
+TEST_CASE("minus equal function unconnected type")
+{
+    CHECK_THROWS(u11 -= u4); //diff type
+    CHECK_THROWS(u4 -= u11);
 }
